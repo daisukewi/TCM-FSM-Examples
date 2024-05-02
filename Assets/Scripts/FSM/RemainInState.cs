26bda06e98fed9cbbf83c3edc84e2cec2ddcdfa5
Dani <daisuke.wi@gmail.com>
2024-05-02 12:44:10 +0200

    Base for FSM examples

--- Assets/Scripts/FSM/RemainInState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "FSM/Remain In State", fileName = "RemainInState")]
-public sealed class RemainInState : BaseState
-{
-    
-}
